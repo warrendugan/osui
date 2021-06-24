91cf5d8a114abce49a20c306f19feee9bbb139cd
Warren Dugan <warrendugan@gmail.com>
2021-06-23 23:36:19 -0700

    bolster ci.

--- libs/dialog/src/lib/dialog.module.ts
+++ libs/dialog/src/lib/dialog.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [CommonModule],
 })
-export class DialogModule {}
+export class DialogModule {
+  constructor() {
+    console.log('test');
+  }
+}
